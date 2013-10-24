3eb8339236b33dfbb0b11272278477417a5bf3ac
SammysHP <sven@sammyshp.de>
2013-10-24 12:44:16 +0200

    Add textclock with locale

--- init.lua
+++ init.lua
@@ -12,6 +12,7 @@ return
     taginfo = require("widgets.taginfo");
     loadavg = require("widgets.loadavg");
     volume = require("widgets.volume");
+    textclock = require("widgets.textclock");
 }
 
 -- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
